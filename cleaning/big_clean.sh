12e1c20d21bd4150b3b06a177731c767ea7a73b3
Jaden Rainey <raine123@d.umn.edu>
2025-08-26 21:16:37 +0000

    added find & grep to delete files with "DELETE ME!" in the temp directory

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,4 +3,8 @@
 tempdir=$(mktemp -d)
 # Extract the tar into the temp directory
 tar -xf big_dir.tgz -C $tempdir
+# move into temp directory
+cd $tempdir
+# use find & grep to delete files with "DELETE ME!"
+find . -type f -exec grep -q 'DELETE ME!' {} \; -delete
 
