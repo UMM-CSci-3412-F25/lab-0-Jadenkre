b0131148e2d6b928ecc65a5a55591f4cc8bcfd92
user <raine123@d.umn.edu>
2025-08-26 20:21:15 +0000

    extract_and_compile.sh passing

---
+++ compiling/extract_and_compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# extract the NthPrime.tgz
+tar -xzf NthPrime.tgz
+# move to the folder we extracted
+cd NthPrime/
+# compile the c files using gcc
+OUTPUT="NthPrime"
+gcc main.c nth_prime.c -o $OUTPUT
+# Grab number from args
+n=$1
+# run the resulting exe
+./$OUTPUT "$n"
+
