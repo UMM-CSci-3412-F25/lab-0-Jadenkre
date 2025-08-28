a5334a3cd6bcdd2d6a5df7325b82fe503215b3f7
Jaden Rainey <raine123@d.umn.edu>
2025-08-28 15:11:44 +0000

    shellcheck adjustments for first script, final

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,7 +2,7 @@
 # extract the NthPrime.tgz
 tar -xzf NthPrime.tgz
 # move to the folder we extracted
-cd NthPrime/
+cd NthPrime/ || exit
 # compile the c files using gcc
 OUTPUT="NthPrime"
 gcc main.c nth_prime.c -o $OUTPUT
