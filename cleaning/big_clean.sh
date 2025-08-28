cb9a5b33724468b66e68a37c51af9cfdd78013d1
Jaden Rainey <raine123@d.umn.edu>
2025-08-28 03:37:11 +0000

    multiple changes reworking the script structure

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# Create temp directory to work in
+# Trap to make sure temp is cleaned even on failure
+trap 'rm -rf "$tempdir"' EXIT
+# save path
+argpath="$(pwd)/$1"
+# Create temp directory
 tempdir=$(mktemp -d)
 # Extract the tar into the temp directory
-tar -xf big_dir.tgz -C $tempdir
-# move into temp directory
+tar -xzf "$argpath" -C "$tempdir"
+# Move into temp dir
 cd $tempdir
 # use find & grep to delete files with "DELETE ME!"
-find . -type f -exec grep -q 'DELETE ME!' {} \; -delete
-
+find . -type f -print0 | xargs -0 grep -l "DELETE ME!" | xargs rm -f
+# return to previous directory
+cd ..
+# create new archive
+newdir="cleaned_$1"
+tar -czf "$newdir" -C "$tempdir"
